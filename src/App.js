e9372ddef4884206a9297dd92167b597dc4ca60b
Luke Matheny <lmatheny277@gmail.com>
2023-06-03 17:38:14 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -4,23 +4,19 @@ import "./App.css";
 import Table from "./components/Table";
 import AdHelp from './adHelp.js'
 import Compiler from "./Compiler/Compiler.js";
-import { isMobile } from 'react-device-detect';
-import Box from '@mui/material/Box';
 import blankAd from './icons/contact.png'
 
 function App() {
 
   
 
-  if (!isMobile) {
+
     return (
       <div>
         <Table />
          <AdHelp /> 
       </div>
     );
-  } else {
-
-  }
+  
 }
 export default App;
--- src/adHelp.js
+++ src/adHelp.js
@@ -1,10 +1,18 @@
 import React, { useState, useRef, useEffect, useCallback } from "react";
 import "./adHelp.css";
 import blankAd from './icons/contact.png'
+import { isMobile } from 'react-device-detect';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Button from '@mui/material/Button';
 
 function AdHelp() {
     const [windowWidth, setWindowWidth] = useState(window.innerWidth)
     const [windowHeight, setWindowHeight] = useState(window.innerHeight)
+    const [open, setOpen] = React.useState(true);
     const setWindowDimensions = () => {
         setWindowWidth(window.innerWidth)
         setWindowHeight(window.innerHeight)
@@ -17,22 +25,68 @@ function AdHelp() {
     }, [])
 
 
-    const [flip, setFlip] = useState(false);
-    return (
+
+    const handleClose = () => {
+        setOpen(false);
+    };
 
 
-        <div>
-            <iframe
-                frameBorder="0"
-                height= "400px"
-                src="https://onecompiler.com/embed/"
-                width="100%"
-            ></iframe>
-            {/* <img style={{ width: '100%', height: (windowHeight / 100) * 20, marginBottom: '0%' }} align='center' src={blankAd} class="adBanner" ></img> */}
-        </div>
+
+    if (!isMobile) {
+        return (
 
 
-    );
+            <div>
+                <iframe
+                    frameBorder="0"
+                    height="400px"
+                    src="https://onecompiler.com/embed/"
+                    width="100%"
+                ></iframe>
+                {/* <img style={{ width: '100%', height: (windowHeight / 100) * 20, marginBottom: '0%' }} align='center' src={blankAd} class="adBanner" ></img> */}
+            </div>
+
+
+        );
+    } else {
+        return (
+
+
+
+            <div>
+                <Dialog
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                >
+                    <DialogTitle id="alert-dialog-title">
+                        {""}
+                    </DialogTitle>
+                    <DialogContent>
+                        <DialogContentText id="alert-dialog-description">
+                            {"Turn your phone to the side for a better experience"}
+                        </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleClose}>CLOSE</Button>
+                    
+                    </DialogActions>
+                </Dialog>
+                <iframe
+                    frameBorder="0"
+                    height="400px"
+                    src="https://onecompiler.com/embed/"
+                    width="100%"
+                ></iframe>
+                {/* <img style={{ width: '100%', height: (windowHeight / 100) * 20, marginBottom: '0%' }} align='center' src={blankAd} class="adBanner" ></img> */}
+            </div>
+
+
+        );
+
+    }
+
 }
 
 export default AdHelp;
